8ccb46752311772d871d9f20dc3064d6f94afaf4
hitchhooker <tommi@niemi.lol>
2024-04-06 21:17:53 +0700

    add sorting

--- scripts/generate_readme.rs
+++ scripts/generate_readme.rs
@@ -12,13 +12,22 @@ fn main() -> io::Result<()> {
         if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("md") {
             let file_name = path.file_name().unwrap().to_str().unwrap();
             let link = format!("- [{}]({}/{})", file_name, proposals_dir, file_name);
-            links.push(link);
+            links.push((file_name.to_string(), link));
         }
     }
 
+    // Sort links based on the leading number in the file name
+    links.sort_by_key(|(file_name, _)| {
+        file_name
+            .split('_')
+            .next()
+            .and_then(|n| n.parse::<i32>().ok())
+            .unwrap_or(0)
+    });
+
     let mut file = fs::File::create(readme_path)?;
     writeln!(file, "# IBP Proposals\n")?;
-    for link in links {
+    for (_, link) in links {
         writeln!(file, "{}", link)?;
     }
 
